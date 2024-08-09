e7d44e2d7fb536285aca725b77d1fe0997272041
ProProperDev <137059053+ProProperDev@users.noreply.github.com>
2024-08-09 18:13:03 +0300

    add item group class

---
+++ logic/src/item_group.cpp
@@ -0,0 +1,6 @@
+#include "../include/item_group.hpp" // TODO add path in Makefile
+
+namespace Store {
+    ItemGroup::ItemGroup(const std::string& name)
+        : name_(name) {}
+} // namespace Store
