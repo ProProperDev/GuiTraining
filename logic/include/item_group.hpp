024b95e5095e2675391f90ccee376337de51c9f3
ProProperDev <137059053+ProProperDev@users.noreply.github.com>
2024-10-08 18:00:21 +0300

    draft channel class + draft timepoints

--- logic/include/item_group.hpp
+++ logic/include/item_group.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
 #include <string>
+#include <chrono>
+#include <list>
+#include <tuple>
 
-namespace Store {
-class ItemGroup {
-    public:
-    ItemGroup(const std::string& name);
-    /*TODO*/
-    ~ItemGroup();
-    private:
-    std::string name_;    
+using years_t = std::chrono::years;
+using months_t = std::chrono::months;
+using days_t = std::chrono::days;
+using hours_t = std::chrono::hours;
+using minutes_t = std::chrono::minutes;
+using seconds_t = std::chrono::seconds;
+
+class TimePoint
+{
+private:
+    std::tuple<months_t, days_t, hours_t, minutes_t, seconds_t> time_point;
 };
+
+namespace Store
+{
+    class Channel
+    {
+    private:
+        std::string channel_name_{""};
+        // thermistor_model_t thermistor_model_{};
+        std::list<std::pair<TimePoint, float>> data_list_;
+    };
 } // namespace Store
