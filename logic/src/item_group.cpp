008134f76150dd516737a8a893f5ee70507ed452
ProProperDev <137059053+ProProperDev@users.noreply.github.com>
2024-10-17 16:01:13 +0300

    Delete logic/src/item_group.cpp

--- logic/src/item_group.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/item_group.hpp" // TODO add path in Makefile
-
-namespace Store {
-    ItemGroup::ItemGroup(const std::string& name)
-        : name_(name) {}
-} // namespace Store
